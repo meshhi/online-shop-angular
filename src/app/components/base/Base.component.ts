4d31fd4dfbf3dda318475a1e7e942457c579c0c0
Michael <yumythug@yandex.ru>
2024-04-14 19:54:43 +0300

    Added folders to structure.

---
+++ src/app/components/base/Base.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+selector: 'Base-component',
+templateUrl: './Base.component.html',
+styleUrls: ['./Base.component.scss']
+})
+export class BaseComponent implements OnInit {
+
+constructor() {
+}
+
+ngOnInit(){
+
+}
+}
---
+++ src/app/components/dialog-box/DialogBox.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+selector: 'DialogBox-component',
+templateUrl: './DialogBox.component.html',
+styleUrls: ['./DialogBox.component.scss']
+})
+export class DialogboxComponent implements OnInit {
+
+constructor() {
+}
+
+ngOnInit(){
+
+}
+}

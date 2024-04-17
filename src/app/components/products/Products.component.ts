44a21b40833fe0f355eeca0af6c369b2c44bf40e
Michael <yumythug@yandex.ru>
2024-04-17 03:02:39 +0300

    cards

--- src/app/components/products/Products.component.ts
+++ src/app/components/products/Products.component.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import { CardComponent } from './card/card.component';
 
 @Component({
   selector: 'Products-component',
   standalone: true,
   templateUrl: './Products.component.html',
   styleUrls: ['./Products.component.scss'],
-  imports: [HttpClientModule],
+  imports: [HttpClientModule, MatCardModule, MatButtonModule, CardComponent],
 })
 export class ProductsComponent implements OnInit {
+  public test: string[] = ['first', 'second', 'third'];
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
--- src/app/components/ui/footer/footer.component.ts
+++ src/app/components/ui/footer/footer.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
 
 @Component({
-selector: 'app-footer',
-standalone: true,
-templateUrl: './footer.component.html',
-styleUrls: ['./footer.component.scss']
+  selector: 'app-footer',
+  standalone: true,
+  imports: [MatIconModule, MatButtonModule, MatToolbarModule, RouterLink],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  constructor() {}
 
-constructor() {
-}
-
-ngOnInit(){
-
-}
+  ngOnInit() {}
 }
